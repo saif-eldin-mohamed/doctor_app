99378f6d262f94c661a4c1677e05aa148b82fccd
Saif mohamed <94307792+ssaiffff@users.noreply.github.com>
2024-06-21 18:58:53 +0300

    splash screen complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,10 @@
+import 'package:doctor_app/core/routing/app_router.dart';
 import 'package:flutter/material.dart';
 
+import 'doc_app.dart';
+
 void main() {
-
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'DOCTOR APP',
-      theme: ThemeData(
-
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-
-      ),
-      home: const MyHomePage(),
-    );
-  }
+  runApp(DocApp(
+    appRouter: AppRouter(),
+  ));
 }
